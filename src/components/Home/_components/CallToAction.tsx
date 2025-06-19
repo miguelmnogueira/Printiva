d956ef116595abb73d0164c9eb89629c8c978a91
miguelmnogueira <miguelmiguelmnogueira@gmail.com>
2025-06-19 12:07:55 -0300

    feat: infinite grid from dev and blur animation from dev

--- src/components/Home/_components/CallToAction.tsx
+++ src/components/Home/_components/CallToAction.tsx
@@ -1,33 +1,40 @@
 import { FaAngleRight } from "react-icons/fa6";
 import { Link } from "react-scroll";
+import { BlurFade } from "@/components/ui/blur-fade";
 
 const CallToAction = () => {
 	return (
 		<div className="font-poppins w-[950px] flex flex-col gap-5">
-			<h1 className="text-6xl font-semibold">
-				Criação, personalização e impressão 3D em um só lugar.
-			</h1>
-			<h3 className="text-2xl max-w-[60%] font-medium bg-linear-to-r from-subtitle to-subtitle/60 bg-clip-text text-transparent">
-				Design, arte e tecnologia trabalhando juntos para materializar o
-				que você imagina.
-			</h3>
-			<div className="flex gap-5 items-center mt-5 font-inter">
-				<Link
-					to={"/contact"}
-					className="flex items-center px-4 h-10 bg-foreground/90 rounded-lg text-primary-foreground font-semibold text-[15px] hover:bg-white transition-colors cursor-pointer"
-				>
-					Inicie seu projeto
-				</Link>
-				<Link
-					to={"features"}
-					smooth={`500`}
-					offset={-130}
-					className="h-10 flex items-center text-primary/65 text-[16px]  gap-2 hover:ml-0.5 hover:gap-2.5 hover:text-primary/90 transition-all"
-				>
-					Veja mais sobre nossos projetos
-					<FaAngleRight />
-				</Link>
-			</div>
+			<BlurFade delay={0.25} duration={1}>
+				<h1 className="text-6xl font-semibold">
+					Criação, personalização e impressão 3D em um só lugar.
+				</h1>
+			</BlurFade>
+			<BlurFade delay={0.25 * 2} duration={1}>
+				<h3 className="text-2xl max-w-[60%] font-medium bg-linear-to-r from-subtitle to-subtitle/60 bg-clip-text text-transparent">
+					Design, arte e tecnologia trabalhando juntos para
+					materializar o que você imagina.
+				</h3>
+			</BlurFade>
+			<BlurFade delay={0.25 * 3} duration={1}>
+				<div className="flex gap-5 items-center mt-5 font-inter">
+					<Link
+						to={"/contact"}
+						className="flex items-center px-4 h-10 bg-foreground/90 rounded-lg text-primary-foreground font-semibold text-[15px] hover:bg-white transition-colors cursor-pointer"
+					>
+						Inicie seu projeto
+					</Link>
+					<Link
+						to={"features"}
+						smooth={`500`}
+						offset={-130}
+						className="h-10 flex items-center text-primary/65 text-[16px]  gap-2 hover:ml-0.5 hover:gap-2.5 hover:text-primary/90 transition-all"
+					>
+						Veja mais sobre nossos projetos
+						<FaAngleRight />
+					</Link>
+				</div>
+			</BlurFade>
 		</div>
 	);
 };
--- src/components/Home/_components/FeaturesSection.tsx
+++ src/components/Home/_components/FeaturesSection.tsx
@@ -1,38 +1,44 @@
 import { Link } from "react-router";
-import { FeaturesSectionWithHoverEffects } from "./feature-section-with-hover-effects";
+import { FeaturesSectionWithHoverEffects } from "../../feature-section-with-hover-effects";
 import { FaAngleRight } from "react-icons/fa";
 import { Element } from "react-scroll";
+import { BlurFade } from "@/components/ui/blur-fade";
 
 const FeaturesSection = () => {
 	return (
 		<section className="flex flex-col gap-20">
-			<div className="flex justify-between w-[90%]">
-				<Element name="features">
-					<h3 className="text-6xl font-poppins font-semibold">
-						Feitos para todas as necessidades
-					</h3>
-				</Element>
-				<div className="w-[70%] text-sm flex items-end text-subtitle">
-					<div>
-						<p>
-							Na Printiva, combinamos precisão técnica,
-							criatividade e agilidade para transformar ideias em
-							realidade. Nosso processo une design, engenharia e
-							personalização com excelência em cada detalhe.
-						</p>
-						<Link
-							to={`/contact`}
-							className="text-white/80 flex items-center gap-0 hover:text-white/90  transition-all duration-200 hover:gap-0.5 hover:ml-0.5"
-						>
-							<span>Comece aqui </span>
-							<FaAngleRight />
-						</Link>
+			<BlurFade delay={0.25 * 3} duration={1} inView>
+				<div className="flex justify-between w-[90%]">
+					<Element name="features">
+						<h3 className="text-6xl font-poppins font-semibold">
+							Feitos para todas as necessidades
+						</h3>
+					</Element>
+					<div className="w-[70%] text-sm flex items-end text-subtitle">
+						<div>
+							<p>
+								Na Printiva, combinamos precisão técnica,
+								criatividade e agilidade para transformar ideias
+								em realidade. Nosso processo une design,
+								engenharia e personalização com excelência em
+								cada detalhe.
+							</p>
+							<Link
+								to={`/contact`}
+								className="text-white/80 flex items-center gap-0 hover:text-white/90  transition-all duration-200 hover:gap-0.5 hover:ml-0.5"
+							>
+								<span>Comece aqui </span>
+								<FaAngleRight />
+							</Link>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className="mb-7 w-full">
-				<FeaturesSectionWithHoverEffects />
-			</div>
+			</BlurFade>
+			<BlurFade delay={0.25 * 4} duration={1} inView>
+				<div className="mb-7 w-full">
+					<FeaturesSectionWithHoverEffects />
+				</div>
+			</BlurFade>
 		</section>
 	);
 };
